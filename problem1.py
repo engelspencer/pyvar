d5afc05420ee9ec7fe6e5b1d451b3790dd8e5bed
engelspencer <sengel@sjnma.org>
2017-09-14 14:32:08 -0500

    problem1.py

---
+++ problem1.py
@@ -0,0 +1,14 @@
+print("Please enter five numbers.")
+n1=input("Number 1: ")
+n1=int(n1)
+n2=input("Number 2: ")
+n2=int(n2)
+n3=input("Number 3: ")
+n3=int(n3)
+n4=input("Number 4: ")
+n4=int(n4)
+n5=input("Number 5: ")
+n5=int(n5)
+total=n1+n2+n3+n4+n5
+avg=total/5
+print(avg)
